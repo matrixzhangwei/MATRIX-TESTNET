d083187e41aa09e698ee27285f2fcafbcd233b75
matrixzhangwei <39425423+matrixzhangwei@users.noreply.github.com>
2018-11-16 12:34:01 +0800

    minor updates to blockgenor.go

--- blockgenor/blockgenor.go
+++ blockgenor/blockgenor.go
@@ -1,3 +1,4 @@
+//1542342839.9728708
 // Copyright 2018 The MATRIX Authors as well as Copyright 2014-2017 The go-ethereum Authors
 // This file is consisted of the MATRIX library and part of the go-ethereum library.
 //
